4d6898f2a8ac099c96a72834e116480baee9bdcd
churchill nnanna <mrxyfactor@gmail.com>
2024-02-19 12:50:13 +0100

    fix 3

--- src/app/pathName.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { usePathname } from "next/navigation"
-
-export default function PathName(){
-    const pathName=usePathname()
-    return(<p>{pathName=='/'? "Home": pathName}</p>)
-}
